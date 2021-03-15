c6df74801f452c64b1c0a644f147a978841338d6
Angel Cruz <angi_ed01@hotmail.com>
2021-03-15 09:51:03 -0600

    Fixing scholarship

--- src/components/forms/Groups.js
+++ src/components/forms/Groups.js
@@ -18,60 +18,11 @@ export const Groups = () => {
                                     <div className="form_inputsContainer form__inputs">
                                         <div className="form__inputs gro__inputsContainer">
                                             <Select contentClassName="gro__inputs-group input__special" label="Grupo" />
+                                        </div>
+                                        <div className="form__inputs gro__inputsContainer">
                                             <Select contentClassName="gro__inputs-major input__special" label="Carrera" />
                                         </div>
-                                        <div className="form__inputs gro__tableContainer ">
-
-                                            <table className="gro__table">
-                                                <thead>
-                                                    <tr className="gro__table-title">
-                                                        <th className="gro__table-name">Nombre del alumno</th>
-                                                        <th className="gro__table-id">Matricula</th>
-                                                        <th className="gro__table-grade">Calificación</th>
-                                                        <th className="gro__table-buttons"></th>
-                                                        <th className="gro__table-buttons"></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    {[
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                        { name: 'Angel Retana ', id: '000000000', grade: '5.0' },
-                                                    ].map(
-                                                        ({ name, id, grade }, i) => (
-                                                            <tr className='gro__table-item' key={i}>
-                                                                <td className="gro__table-name">{name}</td>
-                                                                <td className="gro__table-id">{id}</td>
-                                                                <td className="gro__table-grade">{grade}</td>
-                                                                <td className="gro__table-buttonEdit">
-                                                                    <button className="btn btn-edit">
-                                                                        <i className="fas fa-edit"></i>
-                                                                    </button>
-
-                                                                </td>
-                                                                <td className="gro__table-buttonDelete">
-
-                                                                    <button className="btn ">
-                                                                        <i className="fas fa-trash"></i>
-                                                                    </button>
-                                                                </td>
-                                                            </tr>
-                                                        )
-
-                                                    )}
-
-                                                </tbody>
-                                            </table>
-                                        </div>
+                                        
                                         {/* <button className="gro__addStudent">
                                             <label> Agregar alumno</label>
                                         </button> */}
--- src/components/forms/Scholarship.js
+++ src/components/forms/Scholarship.js
@@ -6,58 +6,48 @@ import { PanelJustAdded } from '../ui/panel/PanelJustAdded'
 export const Scholarship = () => {
     return (
         <div className="containerSection form__container">
-        <div className="form__container-content">
-            <div className="form__container-content-inf scholar__container-content-inf">
-                <BackTexture />
-                <div className="form_containerSection">
-                    <form className="form__form" action="">
-                        <div className="scholar__personal form_section">
-                            <div className="overTexture ">
-                                <div className="form_inputsContainer">
-                                    <div className="form__inputs scholar__special">
-                                        <Input  contentClassName="scholar__inputs-id" 
-                                        label="Maricula" 
-                                        />
-                                    </div>
-                                    <div className="form__inputs scholar__special">
-                                        <Input  contentClassName="scholar__inputs-student" 
-                                        label="Becario" 
-                                        />
-                                    </div>
-                                    <div className="form__inputs scholar__special">
-                                        <Input  contentClassName="scholar__inputs-scholarname" 
-                                        label="Nombre de la beca" 
-                                        />
-                                    </div>
-                                    <div className="form__inputs scholar__special">
-                                        <Input  contentClassName="scholar__inputs-about" 
-                                        label="Razón de beca" 
-                                        />
-                                    </div>
-                                    <div className="form__inputs scholar__special">
-                                        <Input  contentClassName="scholar__inputs-percentaje" 
-                                        type="range"
-                                        min="1"                    // default 0
-                                        max="100"                  // default 100
-                                        step="1"                   // default 1
-                                        value="100"                 // default min + (max-min)/2
-                                        // default horizontal
-                                        label="Porcentaje de beca" 
-                                        />
-                                    </div>
-                                    <div className="form__inputs scholar__special">
-                                        <Input  contentClassName="scholar__inputs-observation" 
-                                        label="Observaciones" 
-                                        />
+            <div className="form__container-content">
+                <div className="form__container-content-inf scholar__container-content-inf">
+                    <div className="scholar__texture"><BackTexture /></div>
+                    <div className="form_containerSection">
+                        <form className="form__form" action="">
+                            <div className="scholar__personal form_section">
+                                <div className="overTexture ">
+                                    <div className="form_inputsContainer">
+                                        <div className="form__inputs form__column  scholar__special">
+                                            <Input contentClassName="scholar__inputs-id"
+                                                label="Maricula"
+                                            />
+                                            <Input contentClassName="scholar__inputs-student"
+                                                label="Becario"
+                                            />
+                                            <Input contentClassName="scholar__inputs-scholarname"
+                                                label="Nombre de la beca"
+                                            />
+                                            <Input contentClassName="scholar__inputs-about"
+                                                label="Razón de beca"
+                                            />
+                                            <Input contentClassName="scholar__inputs-percentaje"
+                                                type="range"
+                                                min="1"                    // default 0
+                                                max="100"                  // default 100
+                                                step="1"                   // default 1
+                                                value="100"                 // default min + (max-min)/2
+                                                // default horizontal
+                                                label="Porcentaje de beca"
+                                            />
+                                            <Input contentClassName="scholar__inputs-observation" inputClassName="scholar__inputs-observationIn"
+                                                label="Observaciones"
+                                            />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
+                <PanelJustAdded />
             </div>
-            <PanelJustAdded />
         </div>
-    </div>
     )
 }
