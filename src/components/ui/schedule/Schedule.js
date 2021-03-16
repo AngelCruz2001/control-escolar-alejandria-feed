42d670054eb5b15585e77ea424de8d92b796399f
Francisco Rafael <rafudo.beltran@gmail.com>
2021-03-16 10:58:15 -0600

    Table design

--- src/components/ui/schedule/Schedule.js
+++ src/components/ui/schedule/Schedule.js
@@ -1,28 +1,60 @@
-import React from 'react'
-import { Select } from '../inputs/Select'
+import React from "react";
+import { Select } from "../inputs/Select";
 
-export const Schedule = ({
-    special = false,
-}) => {
-    return (
-        <div className="sch__container">
-            <div className="sch__daysItems">
-                <label className="sch__titleDays">Horario:</label>
-                {
-                    ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'].map((day, i) => (
-                        <div className="sch__card" key={i}>
-                            <label className="">{day}</label>
-                        </div>
-                    ))
-                }
-            </div>
-            <div className="sch__time">
-                <div className="sch__selectors">
-                    <Select contentClassName="sch__inputs-from" label="De:" nexTo={true} />
-                    <Select contentClassName="sch__inputs-to" label="A:" nexTo={true} />
-                </div>
-                <label className="sch__labelDay"> Lunes</label>
-            </div>
-        </div>
-    )
+export const Schedule = ({ special = false }) => {
+  return (
+    <div className="sch__container">
+      <label className="sch__schedule">
+        Horario: <span className="sch__schedule-span">(Seleccione día(s) y hora)</span>
+      </label>
+      <table className="sch__table">
+        <thead className="sch__table-days">
+          <tr className="sch__table-days-items">
+            {[
+              "Lunes",
+              "Martes",
+              "Miercoles",
+              "Jueves",
+              "Viernes",
+              "Sabado",
+              "Domingo"
+            ].map((day, i) => (
+              <th key={i}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="sch__time">
+            {[1, 2, 3, 4, 5, 6, 7].map(i => (
+              <td className="sch__selectors">
+                <Select
+                  contentClassName="sch__inputs-from"
+                  label="De:"
+                  nexTo={true}
+                />
+                <Select
+                  contentClassName="sch__inputs-to"
+                  label="A:"
+                  nexTo={true}
+                />
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+{
+  /* <div className="sch__time">
+  <div className="sch__selectors">
+    <Select
+      contentClassName="sch__inputs-from"
+      label="De:"
+      nexTo={true}
+    />
+    <Select contentClassName="sch__inputs-to" label="A:" nexTo={true} />
+  </div>
+</div> */
 }
