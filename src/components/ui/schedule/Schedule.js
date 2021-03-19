4f6281b3f1a85a7ccaaaa446017a39490792873b
Francisco Rafael <rafudo.beltran@gmail.com>
2021-03-18 19:40:19 -0600

    CSS DONE

--- src/components/ui/schedule/Schedule.js
+++ src/components/ui/schedule/Schedule.js
@@ -3,14 +3,16 @@ import { Select } from "../inputs/Select";
 
 export const Schedule = ({ special = false }) => {
 
-  const refDayWeek = useRef(null)
-  const [dayClicked, setDayClicked] = useState(false)
+  const refDayWeek = useRef(null);
+
+  const [dayClicked, setDayClicked] = useState(false);
+
   const handleDayClick = ( {target} ) => {
-    console.log(target.className);
-    
+    console.log(target.className);    
     setDayClicked(!dayClicked);
     refDayWeek.current.className = dayClicked ? "sch__table-item-day" : "sch__table-item-day sch__inputchecked";
   }
+
   return (
     <div className="sch__container">
       <label className="sch__schedule">
@@ -30,7 +32,7 @@ export const Schedule = ({ special = false }) => {
           <div className="sch__table-item">
             <div className="sch__table-item-day" key={i} id="check" ref={refDayWeek} onClick={handleDayClick}>
               <input type="checkbox" id="day" value={day}/>
-              <label for="day" className="sch__label">{day}</label>
+              <label className="sch__label">{day}</label>
             </div>
           </div>
         ))}
