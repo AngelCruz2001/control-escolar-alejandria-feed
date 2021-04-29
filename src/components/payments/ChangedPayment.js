d71fb64fd862a6603358b83cff0921ef3757780f
Angel Cruz <angi_ed01@hotmail.com>
2021-04-28 19:31:03 -0500

    first css payments

---
+++ src/components/payments/ChangedPayment.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const ChangedPayment = () => {
+    return (
+        <>
+            <div className="payment__sectionsChangedItem">
+                <h4>Realizar Pago</h4>
+                <hr />
+            </div>
+            <div className="payment__sectionsChangedItem">
+                <h4>Consultar estado</h4>
+                <hr />
+            </div>
+            <div className="payment__sectionsChangedItem">
+                <h4>Abonos</h4>
+                <hr />
+            </div>
+        </>
+    )
+}
